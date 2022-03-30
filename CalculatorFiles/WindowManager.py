2db2d7cf69ac1027c3b2c93c471054b7d3666e73
LOogann <89930162+LOogann@users.noreply.github.com>
2022-03-30 09:24:29 -0400

    Fixed Merge Issues

--- CalculatorFiles/WindowManager.py
+++ CalculatorFiles/WindowManager.py
@@ -5,20 +5,13 @@ from tkinter import *
 
 #Functions Used when creating a window
 def createWindow(w,h, color, name):
-<<<<<<< HEAD
+
     #iconimage = None
     calc_window = Tk()
     calc_window.title(name)
     calc_window.configure(width= w, height= h, bg= color)
     #calc_window.iconphoto(False, PhotoImage(file = iconimage))
-=======
-    iconimage = r"C:\Users\logan\OneDrive\New Documents\Programming\Vs-Code-Projects\GitProjects\CalculatorWindows\Calculator-Project\Pictures\calculator.png"
-    calc_window = Tk()
-    calc_window.title(name)
-    calc_window.configure(width= w, height= h, bg= color)
-    #icon definer
-    calc_window.iconphoto(False, PhotoImage(file = iconimage))
->>>>>>> 1fd2a7d8d95d60f6f00ffa20c4b6e8bcdc277a7f
+
     calc_window.mainloop()
 
 #the call to create a window
