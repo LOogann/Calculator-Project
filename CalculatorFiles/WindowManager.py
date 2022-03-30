06cefb7e92b05c50606c04fca853bd48180a92b3
LOogann <89930162+LOogann@users.noreply.github.com>
2022-03-30 09:20:39 -0400

    Added Icon For Mac

--- CalculatorFiles/WindowManager.py
+++ CalculatorFiles/WindowManager.py
@@ -3,9 +3,11 @@ import printStarterStatement
 from tkinter import *
 
 def createWindow(w,h, color, name):
+    #iconimage = None
     calc_window = Tk()
     calc_window.title(name)
     calc_window.configure(width= w, height= h, bg= color)
+    #calc_window.iconphoto(False, PhotoImage(file = iconimage))
     calc_window.mainloop()
 
 createWindow(w= 1920, h= 1080, name= "Logan's Calculator", color= "lightgrey")
