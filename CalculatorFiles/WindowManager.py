1fd2a7d8d95d60f6f00ffa20c4b6e8bcdc277a7f
LOogann <89930162+LOogann@users.noreply.github.com>
2022-03-29 22:01:51 -0400

    Added An Icon To The Window

--- CalculatorFiles/WindowManager.py
+++ CalculatorFiles/WindowManager.py
@@ -2,10 +2,16 @@
 import printStarterStatement
 from tkinter import *
 
+
+#Functions Used when creating a window
 def createWindow(w,h, color, name):
+    iconimage = r"C:\Users\logan\OneDrive\New Documents\Programming\Vs-Code-Projects\GitProjects\CalculatorWindows\Calculator-Project\Pictures\calculator.png"
     calc_window = Tk()
     calc_window.title(name)
     calc_window.configure(width= w, height= h, bg= color)
+    #icon definer
+    calc_window.iconphoto(False, PhotoImage(file = iconimage))
     calc_window.mainloop()
 
-createWindow(w= 1920, h= 1080, name= "Logan's Calculator", color= "lightgrey")
+#the call to create a window
+createWindow(w= 1920, h= 1080, name= "Logan's Calculator", color= "#062ebf")
