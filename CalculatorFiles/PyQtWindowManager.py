789a422f0ffc00a4d56f7dae18d5c46607ee312e
LOogann <89930162+LOogann@users.noreply.github.com>
2022-03-31 13:31:33 -0400

    States and Organization

--- CalculatorFiles/PyQtWindowManager.py
+++ CalculatorFiles/PyQtWindowManager.py
@@ -1,25 +1,72 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
 from PyQt5.QtWidgets import *
-#Initializes the Window
+class state:
+    NotImplemented
+
+currentNumber = 4
+currentState = None
+
+addState = state()
+subState = state()
+mulState = state()
+divState = state()
+
+
+#=================================================================
+#Initializes the Window and Neccesary Variables
 app = QApplication([])
-app.setStyle('Macintosh')
+app.setStyle('Fusion')
 window = QWidget()
 layout = QVBoxLayout()
+palette = QPalette()
+palette.setColor(QPalette.ButtonText, Qt.cyan)
 
-# topNumberLabel = QLabel('0000')
-# topNumberLabel.show()
+#defines Qwidget variables
+TopText = QLabel("    0010")
+addButton = QPushButton(text = "   +   ")
+subButton = QPushButton(text = "   _   ")
+debugText = QLabel(str(addState))
+#=================================================================
+#Layout design and Order
+#put in order top to bottom
 
-layout.addWidget(QLabel("0000"))
-layout.addSpacing(50)
+layout.addWidget(TopText)
+layout.addSpacing(10)
 
-layout.addWidget(QPushButton(text = "Click To Click"))
-layout.addSpacing(100)
+layout.addWidget(addButton)
+layout.addSpacing(2)
 
-layout.addWidget(QPushButton(text = "Click To Click To Click"))
+layout.addWidget(subButton)
+layout.addSpacing(10)
 
+layout.addWidget(debugText)
+#=================================================================
+#Button events
+def addButtonEvent():
+    print("Add")
+    TopText.setText(str(currentNumber))
+    currentState = addState
+    debugText.setText(str(currentState))
+def subButtonEvent():
+    print("subtract")
+    TopText.setText(str(-currentNumber))
+    currentState = subState
+    debugText.setText(str(currentState))
+
+addButton.clicked.connect(addButtonEvent)
+subButton.clicked.connect(subButtonEvent)
+
+#=================================================================
+#Finalizing Events
 window.setLayout(layout)
+app.setStyleSheet("QPushButton { margin: 5ex; }")
+app.setPalette(palette)
 
 #shows the window
 window.show()
 #Loops the Window Until Close
 app.exec()
+
+#=================================================================     
